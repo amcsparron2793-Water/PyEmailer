8a5ea7774b4ac52a936dab3b78ca4bd81a517698
Andrew McSparron <Amcsparron@albanyny.gov>
2026-09-23 15:01:35 -0400

    Remove obsolete TODO comment regarding non-monitoring email sending in `ContinuousMonitorAlertSend`.

--- PyEmailerAJM/continuous_monitor/continuous_monitor_alert_send.py
+++ PyEmailerAJM/continuous_monitor/continuous_monitor_alert_send.py
@@ -15,8 +15,6 @@ if TYPE_CHECKING:
 NO_COLORIZER = False
 
 
-# TODO: create a version that does not monitor the inbox and only sends emails when triggered
-#  IE get rid of _AlertMsg etc for this new class - see HTTPServerMonitorEmail
 class ContinuousMonitorAlertSend(ContinuousMonitor):
     """
     Handles sending email alerts for continuous monitoring functionality.
